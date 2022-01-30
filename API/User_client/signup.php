abf9710d1b4791888be34213bd6624f298317910
shahzaib55 <zaib8069@gmail.com>
2022-01-30 08:28:38 +0500

    updated 1/30/2022 8:25

---
+++ API/User_client/signup.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../config/database.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+ 
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata)){
+    $request = json_decode($postdata);
+     
+     
+    $user_firstname = $request->user_firstname;
+    $user_lastname = $request->user_lastname;
+    $user_email = $request->user_email;
+    $user_mobileno = $request->user_mobileno;
+    $user_password = $request->user_password;
+    $sql = "INSERT INTO users (user_firstname, user_lastname, user_email, user_mobileno, user_password) VALUES ('$user_firstname','$user_lastname','$user_email','$user_mobileno','$user_password')";
+    if(mysqli_query($conn,$sql)){
+    
+        echo json_encode(["success"=>true,"msg"=>"inserted"]);
+		return;
+    }
+    else{
+        echo json_encode(["success"=>false,"msg"=>"Failed"]);
+		return;
+         
+    }
+     
+}
+else{
+    echo json_encode(["success"=>false,"msg"=>"Please fill all the required fields!"]);
+    return;
+     
+}
+
+?> 
