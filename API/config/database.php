b6f79f0890632b32b22321a571e3fc1865910eb4
shahzaib55 <zaib8069@gmail.com>
2022-02-12 23:33:05 +0500

    updated 11:32

--- API/config/database.php
+++ API/config/database.php
@@ -4,10 +4,14 @@ header("Access-Control-Allow-Headers: access");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$servername = "databases.000webhost.com";
-$username = "id18414943_root";
-$password = "Database123@";
-$database= "id18414943_beautiproducts";
+// $servername = "databases.000webhost.com";
+// $username = "id18414943_root";
+// $password = "Database123@";
+// $database= "id18414943_beautiproducts";
+$servername = "localhost:3307";
+$username = "root";
+$password = "";
+$database= "databas";
  
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -15,8 +19,6 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}else{
-  echo"connected successfuly";
 }
 
 ?>
--- API/product/delete_product.php
+++ API/product/delete_product.php
@@ -5,10 +5,23 @@ require_once '../config/database.php';
 
 $data = json_decode(file_get_contents("php://input"));  
 	$product_id = $data->product_id;
+    $query = "SELECT product_image FROM product WHERE product_id= '$product_id' ";
+	$exeSQL = mysqli_query($conn, $query);
+    $row = mysqli_fetch_array($exeSQL);
+	$filePath = $row['product_image'];
 	$sql = "DELETE FROM product WHERE product_id= '$product_id' ";
     
     
     if(mysqli_query($conn, $sql)){ 
+		if (file_exists($filePath)) 
+               {
+                 unlink($filePath);
+                  
+              }
+              else
+              {
+               echo "File does not exists"; 
+              }
 		echo json_encode(["success"=>true]);
 		return;
 	}
