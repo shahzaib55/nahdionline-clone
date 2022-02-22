ec2691416098f2edbd0dd30cf03f61b28b1b8171
shahzaib55 <zaib8069@gmail.com>
2022-02-22 19:29:19 +0500

    updated

--- API/User_customer/signup.php
+++ API/User_customer/signup.php
@@ -6,8 +6,6 @@ require_once '../config/database.php';
 $postdata = file_get_contents("php://input");
 if(isset($postdata) && !empty($postdata)){
     $request = json_decode($postdata);
-     
-     
     $user_firstname = $request->user_firstname;
     $user_lastname = $request->user_lastname;
     $user_email = $request->user_email;
@@ -15,7 +13,8 @@ if(isset($postdata) && !empty($postdata)){
     $user_password = $request->user_password;
 
     //insert data query
-    $sql = "INSERT INTO user_customer (user_firstname, user_lastname, user_email, user_mobileno, user_password) VALUES ('$user_firstname','$user_lastname','$user_email','$user_mobileno','$user_password')";
+    $sql = "INSERT INTO user_customer (user_firstname, user_lastname, user_email, user_mobileno, user_password) 
+    VALUES ('$user_firstname','$user_lastname','$user_email','$user_mobileno','$user_password')";
     if(mysqli_query($conn,$sql)){
     
         echo json_encode(["success"=>true,"msg"=>"inserted"]);
