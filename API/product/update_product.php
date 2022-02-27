3ff6c59dd647de0077f49cd8008019a9e2249072
shahzaib55 <zaib8069@gmail.com>
2022-02-27 10:39:55 +0500

    updated

--- API/product/update_product.php
+++ API/product/update_product.php
@@ -1,7 +1,8 @@
 <?php
+//include connection file
 require_once '../config/database.php';
 
- 
+ //get data from json file
 $postdata = file_get_contents("php://input");
 if(isset($postdata) && !empty($postdata)){
     $request = json_decode($postdata);
@@ -9,66 +10,40 @@ if(isset($postdata) && !empty($postdata)){
     $product_id = $request->product_id;
     $product_name = $request->product_name;
     $product_price = $request->product_price;
-    $product_image = $request->upload;
+    $product_image = $request->product_image;
     $product_quantity = $request->product_quantity;
     $product_usefor = serialize($request->product_usefor);
     $product_description = $request->product_description;
     $category_id = $request->category_id;
 
+
     //fetch old image path
-    $query = "SELECT product_image FROM product WHERE product_id= '$product_id' ";
+    $query = "SELECT * FROM product WHERE product_id= '$product_id' ";
 	$exeSQL = mysqli_query($conn, $query);
     $row = mysqli_fetch_array($exeSQL);
 	$old_Image_Path = $row['product_image'];
+    unlink($old_Image_Path);
 
-    //compare with new image path
-    if($product_image != $old_Image_Path){     
-        //delete old image from images folder
-        if (file_exists($old_Image_Path)) 
-               {
-                   
-
-                 unlink($old_Image_Path);
-                 
-                 //update image in images folder
-                 $extension = pathinfo($_FILES['upload']['name'], PATHINFO_EXTENSION);
-                 $new_image_name = time() . "." . $extension;
-                 move_uploaded_file($_FILES['upload']['tmp_name'],'/images/' . $new_image_name);
-                 $image_path = "/images/" . $new_image_name;
-                
-                  //update with new image
-                 $sql = "UPDATE product SET product_name='$product_name',product_price='$product_price',product_image='$image_path'
-                 ,product_quantity='$product_quantity',product_usefor='$product_usefor', product_description='$product_description', category_id='$category_id' WHERE product_id='$product_id'";
-                 if(mysqli_query($conn,$sql)){
-                     echo json_encode(["success"=>true,"msg"=>"updated"]);
-                     return;
-                 }
-                 else{
-                     echo json_encode(["success"=>false,"msg"=>"failed"]);
-                     return;
-                      
-                 }
-              }else{
-                echo json_encode(["success"=>false,"msg"=>"image not exist"]);
-              }
-              //update with old image     
-}else{
-    $qry = "UPDATE product SET product_name='$product_name',product_price='$product_price',product_image='$product_image'
-                 ,product_quantity='$product_quantity',product_usefor='$product_usefor', product_description='$product_description', category_id='$category_id' WHERE product_id='$product_id'";
-                 if(mysqli_query($conn,$qry)){
-                     echo json_encode(["success"=>true,"msg"=>"updated"]);
-                     return;
-                 }
-                 else{
-                     echo json_encode(["success"=>false,"msg"=>"failed"]);
-                     return;   
-                 }
-                   }
+    //update data query
+    $sql = "UPDATE product SET product_name='$product_name',product_price='$product_price',product_image='$product_image'
+    ,product_quantity='$product_quantity',product_usefor='$product_usefor', product_description='$product_description', category_id='$category_id' WHERE product_id='$product_id'";
+    //check if the data is updated
+    if(mysqli_query($conn,$sql)){
+ 
+        echo json_encode(["success"=>true,"msg"=>"updated"]);
+		return;
+    }
+    else{
+        echo json_encode(["success"=>false,"msg"=>"failed"]);
+		return;
+         
+    }
      
-             }else{
-                echo json_encode(["success"=>false,"msg"=>"Please fill all the required fields!"]);
-                return;
-                  }
+}
+else{
+    echo json_encode(["success"=>false,"msg"=>"Please fill all the required fields!"]);
+    return;
+     
+}
+
 ?> 
-
-
