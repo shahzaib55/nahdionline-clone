dfe822aa1b7b9babce6bd21806150e23ef98c2f2
shahzaib <zaib88069@gmail.com>
2022-03-13 20:09:29 +0500

    updated

--- API/Orders/update_order.php
+++ API/Orders/update_order.php
@@ -8,12 +8,12 @@ if(isset($postdata) && !empty($postdata)){
     $request = json_decode($postdata);
      
     $order_id = $request->order_id;
-    $product_id = serialize($request->product_id);
-    $product_quantity = serialize($request->product_quantity);
-    $price = $request->price;
+    $product_detail= json_encode($request->product_detail);
+    $user_detail = json_encode($request->user_detail);
+    $bill = $request->bill;
    
  //update data query
-    $sql = "UPDATE orders SET product_id='$product_id', product_quantity='$product_quantity', price='$price'  WHERE order_id='$order_id'";
+    $sql = "UPDATE orders SET product_detail='$product_detail', user_detail='$user_detail', total_bill='$bill'  WHERE order_id='$order_id'";
     if(mysqli_query($conn,$sql)){
  
         echo json_encode(["success"=>true,"msg"=>"updated"]);
