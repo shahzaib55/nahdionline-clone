2e0043c6af17d68b8fcc486e79e8451fd1c00abe
shahzaib <zaib88069@gmail.com>
2022-03-13 20:05:10 +0500

    updated

--- API/Orders/show_order.php
+++ API/Orders/show_order.php
@@ -2,29 +2,86 @@
 //include connection file
 require_once '../config/database.php';
 
+ //get data from json file
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata)){
+    $request = json_decode($postdata);
+     
+     
+  //GET data from json file
+    $product_detail[] = $request->product_detail;
+    $user_id = $request->user_id;
+    $bill = 0;
 
-//select data query
-	$sql = "SELECT * FROM orders ORDER BY order_id ASC";
-    $exeSQL = mysqli_query($conn, $sql);
+    //fetch carts only for specific user
+    $sqlquery = "SELECT * FROM product_cart WHERE user_id='$user_id'";
+    $exeSQL = mysqli_query($conn, $sqlquery);
 		if(mysqli_num_rows($exeSQL) > 0){
-			while($row_order = mysqli_fetch_array($exeSQL)){ 
+			while($row_cart = mysqli_fetch_array($exeSQL)){ 
 
 				//store data into array
-				$json_array[] = array(
-				  'order_id' =>  $row_order ['order_id'],
-                  'product_id' =>  unserialize($row_order ['product_id']),
-				  'product_quantity' => unserialize($row_order ['product_quantity']),
-				  'price' =>  $row_order ['price'],
-				  'user_detail' => unserialize($row_order['user_detail']),
-				  'date' => $row_order['date']
-				  
-				);
+                $bill = $bill + $row_cart['total_bill'];
+				
 			}
-			echo json_encode(["success"=>true,"fetchorder"=>$json_array]);
-			return;
-		}
-		else{
-			echo json_encode(["success"=>false]);
-			return;
-		}
-?>
+        }
+  //fetch user information
+    $query = "SELECT * FROM user_customer WHERE user_id='$user_id'";
+    $querySQL=mysqli_query($conn,$query);
+    if(mysqli_num_rows($querySQL) > 0){
+
+        $row = mysqli_fetch_array($querySQL);
+
+        //create user detail array
+        $user_detail[] = array(
+            'user_id' => $user_id,
+            'user_firstname' =>  $row['user_firstname'],
+            'user_address' =>  $row['user_address']
+          );
+        
+
+        
+        //convert user_detail array into json form
+        $user_detail = json_encode($user_detail);
+
+        //convert product_detail array into json form
+          $product_detail=json_encode($product_detail);
+        //store data into database
+        $sql = "INSERT INTO orders(product_detail, user_detail, total_bill) 
+        VALUES ('$product_detail','$user_detail','$bill')";
+        if(mysqli_query($conn,$sql)){
+     
+            $qry = "DELETE FROM product_cart WHERE user_id= '$user_id' ";
+          
+    
+            if(mysqli_query($conn, $qry )){
+                
+                //start session of total bill 
+                session_start();
+                $_SESSION["bill"] = $bill;
+
+                //move to paypal integration
+                header("Location: http://localhost/API/paypal_integration/request.php");
+	        }
+           
+            
+        }
+        else{
+            echo json_encode(["success"=>false,"msg"=>"failed"]);
+            return;
+        } 
+
+    }else{
+        echo json_encode(["success"=>true,"msg"=>"record not found"]);
+    }
+
+
+
+    
+}
+ else{
+    echo json_encode(["success"=>false,"msg"=>"Please fill all the required fields!"]);
+	return;
+}  
+
+
+?> 
