3bfdd6050f206560aeee71356dc1866da027ad3b
fnawaz953 <nawazfaisal@rapidzzsolutions.com>
2022-03-15 15:47:02 +0500

    public testing

--- public/config/database.php
+++ public/config/database.php
@@ -1,24 +1,26 @@
 <?php
 //include headers
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT');
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: access");
+// header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT');
+// header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$servername = "databases.000webhost.com";
-$username = "id18414943_root";
-$password = "Database123@";
-$database= "id18414943_beautiproducts";
+// $servername = "databases.000webhost.com";
+// $username = "id18414943_root";
+// $password = "Database123@";
+// $database= "id18414943_beautiproducts";
 
  
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password, $database);
  
-// Check connection
-if ($conn->connect_error) {
-  echo json_encode(["success"=>true]);
-  // die("Connection failed: " . $conn->connect_error);
-}
+// // Check connection
+// if ($conn->connect_error) {
+//   echo json_encode(["success"=>true]);
+//   return
+//   // die("Connection failed: " . $conn->connect_error);
+// }
+echo "Hello connect";
 
 ?>
 
