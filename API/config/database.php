6d02d2ebab4aaa6f9593d48487a3ca4c71484fb1
fnawaz953 <nawazfaisal@rapidzzsolutions.com>
2022-03-23 15:11:24 +0500

    DELETE ADDED in methods

--- API/config/database.php
+++ API/config/database.php
@@ -2,7 +2,7 @@
 //include headers
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT,DELETE');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
   
