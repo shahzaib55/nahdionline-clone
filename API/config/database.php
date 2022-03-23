d3fb7451b989ebe62f01106af8cb4581619da810
fnawaz953 <nawazfaisal@rapidzzsolutions.com>
2022-03-24 01:46:13 +0500

    herrko

--- API/config/database.php
+++ API/config/database.php
@@ -1,6 +1,7 @@
 <?php
 //include headers
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin', 'http://localhost:3000');
+header('Access-Control-Allow-Credentials', 'true');
 header("Access-Control-Allow-Headers: access");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT,DELETE');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
