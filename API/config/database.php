4e1444eea8561b489cd60a30432b005cf9eaad1f
fnawaz953 <nawazfaisal@rapidzzsolutions.com>
2022-03-24 01:44:50 +0500

    paypal

--- API/config/database.php
+++ API/config/database.php
@@ -1,9 +1,9 @@
 <?php
 //include headers
-header('Access-Control-Allow-Origin', '*');
-header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT,DELETE');
-
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
   
 $servername = "remotemysql.com:3306";
