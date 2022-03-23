f99b35568f6a10c289cdbe33672fbe7926c5433a
fnawaz953 <nawazfaisal@rapidzzsolutions.com>
2022-03-23 16:33:12 +0500

    user _id added

--- API/User_customer/show_user.php
+++ API/User_customer/show_user.php
@@ -13,11 +13,12 @@ require_once '../config/database.php';
 
 				//store data into array
 				$json_array[] = array(
+				  'user_id' =>  $row_user ['user_id'],
 				  'user_firstname' =>  $row_user ['user_firstname'],
 				  'user_lastname' =>  $row_user ['user_lastname'],
 				  'user_email' =>  $row_user ['user_email'],
                   'user_mobileno' =>  $row_user ['user_mobileno'],
-                  'user_password' =>  $row_user ['user_password']
+
 				);
 			}
 			echo json_encode(["success"=>true,"fetchUser"=>$json_array]);
