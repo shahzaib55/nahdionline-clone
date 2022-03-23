f402ddf5a1a6710a467e6d177994d4fdc2240f04
fnawaz953 <nawazfaisal@rapidzzsolutions.com>
2022-03-24 01:41:41 +0500

    paypal

--- API/config/database.php
+++ API/config/database.php
@@ -1,7 +1,6 @@
 <?php
 //include headers
 header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Credentials', 'true');
 header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT,DELETE');
 
