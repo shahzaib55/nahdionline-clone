2b5ab2a950c4b224366a494787a0c519b13b3309
fnawaz953 <nawazfaisal@rapidzzsolutions.com>
2022-03-26 12:55:51 +0500

    Thannk-you

--- API/paypal_integration/success.php
+++ API/paypal_integration/success.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once '../config/database.php';
-   header("Location:http://localhost:3000/?success=true");
+   header("Location:http://localhost:3000/thank-you");
 echo json_encode(["success"=>true,"msg"=>"transaction succeeded"]);
 ?>
