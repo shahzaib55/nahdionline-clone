3e39d28ad3f018e12f06b733cca55cff0e1c2281
fnawaz953 <nawazfaisal@rapidzzsolutions.com>
2022-04-06 22:49:48 +0500

    filter orute

--- API/product/select_product.php
+++ API/product/select_product.php
@@ -20,7 +20,7 @@ require_once '../config/database.php';
 	//check if any record found
 		if(mysqli_num_rows($exeSQL) > 0){
 			while($row_product = mysqli_fetch_array($exeSQL)){ 
-			      $usefor= explode(",",$row_product['product_usefor']);
+			      $usefor= json_decode([$row_product['product_usefor']]);
                 
    echo json_encode(["success"=>true,"fetchproduct"=>$row_product['product_usefor']]);
 			return;
